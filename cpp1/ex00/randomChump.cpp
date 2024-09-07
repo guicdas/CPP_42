0ac68b7b859c673666dd87ce71723b41c429adca
Guilherme Silva <silva.caldeira.guilherme@gmail.com>
2024-09-07 16:45:24 +0100

    submodules

--- cpp1/ex00/randomChump.cpp
+++
@@ -1,6 +0,0 @@
-#include "Zombie.hpp"
-
-void randomChump( std::string name ){
-	Zombie Z(name);
-	Z.announce();
-}
--- cpp1/ex01/zombieHorde.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* zombieHorde( int zNumber, std::string name ){
-	Zombie *z = new Zombie[zNumber];
-	for ( int i =0 ; i < zNumber; i++)
-		z[i].setName(name, i);
-	return (z);
-}
